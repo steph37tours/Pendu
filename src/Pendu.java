b36bffceba744443e1844cb8d98d54615bc049ea
steph37tours <stephane.curet@orange.fr>
2023-02-04 17:07:36 +0100

    version fonctionnelle avec rejouer

--- src/Pendu.java
+++ src/Pendu.java
@@ -6,71 +6,93 @@ public class Pendu {
 
 	public static void main(String[] args) {
 		System.out.println("jeu de pendu");
+		String rejouer = "O";
+
 		// dans un premier temps, un mot simple
 		String motChoisi = "TOTO";
 		int position;
-		int nbcar = 0;
-		int nbPerdu = 0;
-		int lettresTrouvees = 0;
-		int j = 0;
-		boolean condSortie = false;
-		boolean succesEchec = false;
+		int nbcar;
+		int nbPerdu;
+		int lettresTrouvees;
+		int j;
+
+		boolean condSortie;
+		boolean succesEchec;
 		String[] tableauResultat = new String[motChoisi.length()];
-
-		// on remplit le tableau
-		for (int i = 0; i < motChoisi.length(); i++) {
-			tableauResultat[i] = "_";
-		}
-
 		do {
-			System.out.println("");
-			System.out.println("Entrez un caractère :");
+			//init
+			condSortie = false;
+			succesEchec = false;
+			nbPerdu = 0;
+			j = 0;
+			lettresTrouvees = 0;
+			nbcar = 0;
 
-			String lettreChoisie = LectureClavier.lireString();
-
-			// on recherche si le caractere choisi est présent plusieurs fois
+			// on remplit le tableau
+			for (int i = 0; i < motChoisi.length(); i++) {
+				tableauResultat[i] = "_";
+			}
 
 			do {
-				position = motChoisi.indexOf(lettreChoisie, j);
-				if (position >= 0) {
-					// On ajoute le caractere à la position dans le tableau
-					tableauResultat[position] = lettreChoisie;
-					nbcar++;
-					// on compte le nombre de lettres trouvées au total
-					lettresTrouvees++;
-					j = position + 1;
+				// on affiche le tableau
+				for (int i = 0; i < motChoisi.length(); i++) {
+					System.out.print(tableauResultat[i]);
 				}
-			} while (position >= 0);
 
-			if (nbcar == 0)
-				nbPerdu++;
+				System.out.println("");
+				System.out.println("Entrez un caractère :");
 
+				String lettreChoisie = LectureClavier.lireString();
+
+				// on recherche si le caractere choisi est présent plusieurs fois
+
+				do {
+					position = motChoisi.indexOf(lettreChoisie, j);
+					if (position >= 0) {
+						// On ajoute le caractere à la position dans le tableau
+						tableauResultat[position] = lettreChoisie;
+						nbcar++;
+						// on compte le nombre de lettres trouvées au total
+						lettresTrouvees++;
+						j = position + 1;
+					}
+				} while (position >= 0);
+
+				if (nbcar == 0)
+					nbPerdu++;
+
+				// on réinitialise les variables :
+				nbcar = 0;
+				j = 0;
+
+				if (lettresTrouvees >= motChoisi.length()) {
+					condSortie = true;
+					succesEchec = true;
+				}
+
+				if (nbPerdu >= 5) {
+					condSortie = true;
+				}
+
+			} while (condSortie == false);
+
+			// on affiche le succès ou l'échec
 			// on affiche le tableau
 			for (int i = 0; i < motChoisi.length(); i++) {
 				System.out.print(tableauResultat[i]);
 			}
 
-			// on réinitialise les variables :
-			nbcar = 0;
-			j = 0;
+			System.out.println("");
+			if (succesEchec == true)
+				System.out.println("Bravo vous avez gagné !");
+			else
+				System.out.println("Perdu!");
 
-			if (lettresTrouvees >= motChoisi.length()) {
-				condSortie = true;
-				succesEchec = true;
-			}
+			System.out.println("Voulez-vous rejouer ? (O/N)");
+			rejouer = LectureClavier.lireString();
 
-			if (nbPerdu >= 5) {
-				condSortie = true;
-			}
+		} while (rejouer.equals("O"));
 
-		} while (condSortie == false);
-
-		// on affiche le succès ou l'échec
-		System.out.println("");
-		if (succesEchec == true)
-			System.out.println("Bravo vous avez gagné !");
-		else
-			System.out.println("Perdu!");
 	}
 
 }
